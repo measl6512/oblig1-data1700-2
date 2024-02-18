351675559f8387e8b5a60d1b35e53a47ebed9aff
measl6512 <measl6512@oslomet.no>
2024-02-18 19:32:43 +0100

    endret kode

--- kinobilletter/script.js
+++ kinobilletter/script.js
@@ -1,70 +1,56 @@
-let billettene = [];
+let billetter = [];
 
-function visBilletter() {
+
+function visBilletter(){
     let kjøptFilm = document.getElementById("filmer").value;
-    let valgteBilletter = document.getElementById("antallBilletter").value;
-    let valgtFornavn = document.getElementById("Fornavn").value;
-    let valgtEtternavn = document.getElementById("Etternavn").value;
-    let valgtTelefonnummer = document.getElementById("Telefonnummer").value;
-    let valgtEpost = document.getElementById("Epost").value;
+    let antallBilletter = document.getElementById("antallBilletter").value;
+    let gittnavn = document.getElementById("fornavn").value;
+    let slektsnavn = document.getElementById("etternavn").value;
+    let tlf = document.getElementById("telefon").value;
+    let mail = document.getElementById("epost").value;
 
     const billett1 = {
         filmer: kjøptFilm,
         antall: antallBilletter,
         fornavn: gittnavn,
         etternavn: slektsnavn,
-        telefonnummer: valgtTelefonnummer,
-        epost: Epost
+        telefon: tlf,
+        epost: mail
     };
 
-    if(billett1.antall === ""){
-        document.getElementById("feilAntall").innerHTML = "Må skrive noe inn i antall";
-    } else{
-        document.getElementById("feilAntall").innerHTML = "";
-    } 
-    if(billett1.fornavn === ""){
-        document.getElementById("feilFornavn").innerHTML = "Må skrive noe inn i fornavn";
-    } else {
-        document.getElementById("feilFornavn").innerHTML = "";
-    } if(billett1.etternavn === ""){
-        document.getElementById("feilEtternavn").innerHTML = "Må skrive noe inn i etternavn";
-    } else {
-        document.getElementById("feilEtternavn").innerHTML = "";
-    }
-    if(billett1.telefonnummer === ""){
-        document.getElementById("feilTelefonnummer").innerHTML = "Må skrive noe inn i telefonnummer";
-    } else {
-        document.getElementById("feilTelefonnummer").innerHTML = "";
-    }
-    if(billett1.epost === ""){
-        document.getElementById("feilEpost").innerHTML = "Må skrive noe inn i epost";
-    } else {
-        document.getElementById("feilEpost").innerHTML = "";
-    }
+    billetter.push(billett1);
+    ut();
 
-    billettene.push(billett1);
-        skrivUt();
+    document.getElementById("filmer").value = "";
+    document.getElementById("antallBilletter").value = "";
+    document.getElementById("fornavn").value = "";
+    document.getElementById("etternavn").value = "";
+    document.getElementById("telefon").value = "";
+    document.getElementById("epost").value = "";
 
-        document.getElementById("filmer").value = "";
-        document.getElementById("antallBillett").value = "";
-        document.getElementById("velgFornavn").value = "";
-        document.getElementById("velgEtternavn").value = "";
-        document.getElementById("velgTelefonnummer").value = "";
-        document.getElementById("velgEpost").value = "";
 }
 
-function skrivUt() {
-    let ut = "";
-    for (let i = 0; i < billettene.length; i++) {
-        ut += billettene[i].film + " " + billettene[i].antall + " " + billettene[i].fornavn
-                + " " + billettene[i].etternavn + " " + billettene[i].telefonnummer +
-                " " + billettene[i].epost + "<br>";
+function ut(){
+    let skrivUt = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefon</th><th>E-post</th></tr>";
+    for(let i = 0; i < billetter.length; i++){
+        skrivUt += "<tr>";
+        skrivUt += "<td>" + billetter[i].filmer + "</td>";
+        skrivUt += "<td>" + billetter[i].antall + "</td>";
+        skrivUt += "<td>" + billetter[i].fornavn + "</td>";
+        skrivUt += "<td>" + billetter[i].etternavn + "</td>";
+        skrivUt += "<td>" + billetter[i].telefon + "</td>";
+        skrivUt += "<td>" + billetter[i].epost + "</td>";
+        skrivUt += "</tr>";
     }
-    document.getElementById("utskrift").innerHTML = ut;
+
+    skrivUt += "</table>";
+    document.getElementById("kjøptBilletter").innerHTML = skrivUt;
+
+
 }
 
-function slettBillett() {
-    billettene = [];
-        console.log(billettene);
-        skrivUt();
+function slettBilletter(){
+    billetter = [];
+    console.log(billetter);
+    ut();
 }
